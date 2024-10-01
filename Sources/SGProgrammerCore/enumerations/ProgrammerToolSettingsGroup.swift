a1c3b1955c279c5ebf6e1a682902a10059f26b81
pclwillmott <pwillmott@northrock.bm>
2024-10-01 14:32:03 +0100

    Image button

--- Sources/SGProgrammerCore/enumerations/ProgrammerToolSettingsGroup.swift
+++ Sources/SGProgrammerCore/enumerations/ProgrammerToolSettingsGroup.swift
@@ -112,7 +112,7 @@ public enum ProgrammerToolSettingsGroup : Int, CaseIterable, Sendable {
     button?.isBordered = false
     button?.tag = self.rawValue
     
-    return button ?? NSButton()
+    return button ?? NSButton(title: self.rawValue, target: nil, action: nil)
     
   }
   
